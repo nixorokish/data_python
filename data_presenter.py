b6cc1c8caae9a3239eb66767de4d0eede09aa938
kayleen <8iscogb9@anonaddy.me>
2022-03-31 14:39:33 -0500

    finished exercises for the day

--- data_presenter.py
+++ data_presenter.py
@@ -1,15 +1,15 @@
 f = open('CupcakeInvoices.csv')
 
-# for row in f:
-#     print(row)
+for row in f:
+    print(row)
 
-# f.seek(0)
+f.seek(0)
 
-# for row in f:
-#     row = row.split(',')
-#     print(row[2])
+for row in f:
+    row = row.split(',')
+    print(row[2])
 
-# f.seek(0)
+f.seek(0)
 
 for row in f:
     row = row.rstrip('\r\n').split(',')
@@ -19,14 +19,14 @@ for row in f:
     # print("{:.2f}".format(x))
     print(x)
 
-# f.seek(0)
+f.seek(0)
 
-# total = 0
+total = 0
 
-# for row in f:
-#     row = row.rstrip('\r\n').split(',')
-#     total = total + float(row[3]) * float(row[4])
+for row in f:
+    row = row.rstrip('\r\n').split(',')
+    total = total + float(row[3]) * float(row[4])
 
-# print(total)
+print(total)
 
-# f.close()
+f.close()
